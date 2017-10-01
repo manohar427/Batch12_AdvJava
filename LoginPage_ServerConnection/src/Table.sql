08ad300f86457cb3d899638fcf5bf945c13c3a45
unknown <manohar427@gmail.com>
2017-10-01 13:15:06 +0530

    Sql file commit

---
+++ LoginPage_ServerConnection/src/Table.sql
@@ -0,0 +1,6 @@
+create table USER(username varchar(10),password varchar(10));
+
+insert into USER values("12345","abc");
+insert into USER values("92345","xyz");
+insert into USER values("17345","def");
+insert into USER values("123765","pqrst");
