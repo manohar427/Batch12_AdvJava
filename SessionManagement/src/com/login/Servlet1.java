dfe982232dba8e072e78e3e3e2dd9c2dfcbb3afa
unknown <manohar427@gmail.com>
2017-10-11 08:27:37 +0530

    Session management

---
+++ SessionManagement/src/com/login/Servlet1.java
@@ -0,0 +1,55 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userName = request.getParameter("username");
+		
+		HttpSession session =  request.getSession();
+		
+		String sessionID = session.getId();
+		
+		System.out.println("Session ID:"+sessionID);
+		
+		session.setAttribute("un", userName);
+		
+		//session.setMaxInactiveInterval(1*60);
+		session.setMaxInactiveInterval(-1);
+		
+		request.getRequestDispatcher("userinfo.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
