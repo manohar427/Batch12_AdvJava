d442b31b3a825d979b0eacedb557f554777c118e
unknown <manohar427@gmail.com>
2017-10-29 23:29:02 +0530

    Bank Project demo

---
+++ Bank/src/g/DBIntializer.java
@@ -0,0 +1,8 @@
+package g;
+
+public interface DBIntializer {
+String DRIVER="com.mysql.jdbc.Driver";
+String CON_STRING="jdbc:mysql://localhost:3306/test";
+String USERNAME="root";
+String PASSWORD="Tester@123";
+}
